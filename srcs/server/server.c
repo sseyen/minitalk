e5f3cb96f9570441f629969c0848a3bb66653ed8
sseyen <krisstalexx@gmail.com>
2025-03-17 17:05:44 +0100

    Fill all the buffer with 0

--- srcs/server/server.c
+++ srcs/server/server.c
@@ -6,7 +6,7 @@
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:19:49 by alisseye          #+#    #+#             */
-/*   Updated: 2025/03/17 16:56:23 by alisseye         ###   ########.fr       */
+/*   Updated: 2025/03/17 17:04:45 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,8 @@ static void	allocate_buff(int signum, char **buff)
 			ft_putendl_fd("Error: malloc failed", 2);
 			exit(1);
 		}
-		(*buff)[0] = 0;
 		i = 0;
-		while (i < size)
+		while (i <= size)
 			(*buff)[i++] = 0;
 		i = 0;
 		size = 0;
